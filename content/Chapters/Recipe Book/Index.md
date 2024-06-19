20d2c2bd83f8196a07bae3b9630f0570cc3d8f48
keybasegit <keybasegit@protonmail.com>
2024-06-19 17:17:06 +0100

    Quartz sync: Jun 19, 2024, 5:17 PM

---
+++ content/Chapters/Recipe Book/Index.md
@@ -0,0 +1,10 @@
+---
+longform:
+  format: scenes
+  title: Recipe Book
+  workflow: Default Workflow
+  sceneFolder: /
+  scenes:
+    - "01"
+  ignoredFiles: []
+---
--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
+    pageTitle: "😋 Recipes for children and grown ups",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
